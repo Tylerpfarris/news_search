68863a5b144646798a2718aa67d613de55462e31
Tyler Farris <Tylerpfarris@gmail.com>
2021-05-13 10:29:39 -0700

    mocked

--- src/container/NewsSearch.test.jsx
+++ src/container/NewsSearch.test.jsx
@@ -4,9 +4,26 @@ import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import NewsSearch from './NewsSearch';
 import userEvent from '@testing-library/user-event';
+import fetch from 'node-fetch';
+
+jest.mock('node-fetch');
 
 describe('NewsSearch Container', () => {
   it('displays a list of articles dependent on search params', async () => {
+    fetch.mockResolvedValue({
+      json: () => [
+        {
+          source: '',
+          image: '',
+          author: '',
+          title: '',
+          description: '',
+          url: '',
+          publishedAt: '',
+          content: '',
+        },
+      ],
+    });
     render(<NewsSearch />);
     await screen.findByText('Loading...');
 
@@ -16,12 +33,14 @@ describe('NewsSearch Container', () => {
     const inputEl = await screen.findByLabelText('Search New Articles');
     userEvent.type(inputEl, 'Biden');
 
-    const submitButton = await screen.findByRole('button', { name: 'search-articles' });
+    const submitButton = await screen.findByRole('button', {
+      name: 'search-articles',
+    });
     userEvent.click(submitButton);
-    
+
     return waitFor(() => {
       const articles = screen.getAllByText('Biden', { exact: false });
       expect(articles).toHaveLength(33);
-    })
+    });
   });
 });
