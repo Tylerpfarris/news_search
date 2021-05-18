eeabedb57bf959ef75aebb4ecef05747ce35c277
Tyler Farris <TylerpFarris@gmail.com>
2021-05-18 12:50:16 -0700

    Revert "msw mock"

--- src/container/NewsSearch.jsx
+++ src/container/NewsSearch.jsx
@@ -5,46 +5,45 @@ import { fetchNewsArticles, fetchNewsBySearch } from '../services/newsApi';
 
 export default class NewsSearch extends Component {
   state = {
-    loading: true,
-    articles: [],
-    queryParams: '',
+      loading: true,
+      articles: [],
+      queryParams: '',
   };
+    
+    async componentDidMount() {
+        const articles = await fetchNewsArticles()
+        this.setState({
+            loading: false,
+            articles,
 
-  async componentDidMount() {
-    const articles = await fetchNewsArticles();
-    this.setState({
-      loading: false,
-      articles,
-    });
-  }
+        });
+    }
+    
+    handleQueryParamsChange = ({ target }) => {
+        this.setState({queryParams: target.value})
+    }
 
-  handleQueryParamsChange = ({ target }) => {
-    this.setState({ queryParams: target.value });
-  };
+    handleSubmit = async (e) => {
+        e.preventDefault();
 
-  handleSubmit = async (e) => {
-    e.preventDefault();
-
-    this.setState({ loading: true });
-    const articles = await fetchNewsBySearch(this.state.queryParams);
-    this.setState({
-      loading: false,
-      articles,
-    });
-  };
+        this.setState({ loading: true });
+        const articles = await fetchNewsBySearch(this.state.queryParams);
+        this.setState({
+            loading: false,
+            articles,
+        })
+    }
 
   render() {
     const { loading, articles, queryParams } = this.state;
-    if (loading) return <h1>Loading...</h1>;
-    return (
-      <>
-        <Search
-          queryParams={queryParams}
-          onQueryParamsChange={this.handleQueryParamsChange}
-          onSubmit={this.handleSubmit}
-        />
+    return loading ? <h1>Loading...</h1> :
+    <>
+            <Search
+                queryParams={queryParams}
+                onQueryParamsChange={this.handleQueryParamsChange}
+                onSubmit={this.handleSubmit}
+            />
         <ArticleList articles={articles} />
-      </>
-    );
+    </>;
   }
 }
