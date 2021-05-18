de2bb82c7f43f9795ed799382c05bd4a5f873505
Tyler Farris <Tylerpfarris@gmail.com>
2021-05-18 12:31:30 -0600

    tobeempty in the waitFor

--- src/container/NewsSearch.test.jsx
+++ src/container/NewsSearch.test.jsx
@@ -29,19 +29,19 @@ describe('NewsSearch Container', () => {
     await screen.findByText('Loading...');
 
     const ulEl = await screen.findByRole('list', { name: 'article list' });
-    expect(ulEl).not.toBeEmptyDOMElement();
-
+    
     const inputEl = await screen.findByLabelText('Search New Articles');
     userEvent.type(inputEl, 'Biden');
-
+    
     const submitButton = await screen.findByRole('button', {
       name: 'search-articles',
     });
     userEvent.click(submitButton);
-
+    
     return waitFor(() => {
       const articles = screen.getAllByText('Biden', { exact: false });
       expect(articles).toHaveLength(31);
+      expect(ulEl).not.toBeEmptyDOMElement();
     });
   });
 });
