d20fbeffeb925fd953ade4a7be2d06e483c2db89
Tyler Farris <Tylerpfarris@gmail.com>
2021-05-18 12:41:58 -0600

    got rid of ternary

--- src/container/NewsSearch.jsx
+++ src/container/NewsSearch.jsx
@@ -5,45 +5,46 @@ import { fetchNewsArticles, fetchNewsBySearch } from '../services/newsApi';
 
 export default class NewsSearch extends Component {
   state = {
-      loading: true,
-      articles: [],
-      queryParams: '',
+    loading: true,
+    articles: [],
+    queryParams: '',
   };
-    
-    async componentDidMount() {
-        const articles = await fetchNewsArticles()
-        this.setState({
-            loading: false,
-            articles,
 
-        });
-    }
-    
-    handleQueryParamsChange = ({ target }) => {
-        this.setState({queryParams: target.value})
-    }
+  async componentDidMount() {
+    const articles = await fetchNewsArticles();
+    this.setState({
+      loading: false,
+      articles,
+    });
+  }
 
-    handleSubmit = async (e) => {
-        e.preventDefault();
+  handleQueryParamsChange = ({ target }) => {
+    this.setState({ queryParams: target.value });
+  };
 
-        this.setState({ loading: true });
-        const articles = await fetchNewsBySearch(this.state.queryParams);
-        this.setState({
-            loading: false,
-            articles,
-        })
-    }
+  handleSubmit = async (e) => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+    const articles = await fetchNewsBySearch(this.state.queryParams);
+    this.setState({
+      loading: false,
+      articles,
+    });
+  };
 
   render() {
     const { loading, articles, queryParams } = this.state;
-    return loading ? <h1>Loading...</h1> :
-    <>
-            <Search
-                queryParams={queryParams}
-                onQueryParamsChange={this.handleQueryParamsChange}
-                onSubmit={this.handleSubmit}
-            />
+    if (loading) return <h1>Loading...</h1>;
+    return (
+      <>
+        <Search
+          queryParams={queryParams}
+          onQueryParamsChange={this.handleQueryParamsChange}
+          onSubmit={this.handleSubmit}
+        />
         <ArticleList articles={articles} />
-    </>;
+      </>
+    );
   }
 }
